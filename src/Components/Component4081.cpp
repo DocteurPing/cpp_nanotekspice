3d1fa2908875947da11413114e19fd1692f040a9
DrPing <cedric.ogire@hotmail.fr>
2018-05-22 16:58:53 +0200

    fix output undefined

--- src/Components/Component4081.cpp
+++ src/Components/Component4081.cpp
@@ -26,22 +26,34 @@ nts::Tristate nts::Component4081::compute(size_t pin)
 		case 3 :
 			this->pins[1] = (links[1].second->compute(links[1].first));
 			this->pins[2] = (links[2].second->compute(links[2].first));
-			to_return = (this->pins[1] == TRUE && this->pins[2] == TRUE) ? TRUE : FALSE;
+			if (this->pins[1] == UNDEFINED || this->pins[2] == UNDEFINED)
+				to_return = UNDEFINED;
+			else
+				to_return = (this->pins[1] == TRUE && this->pins[2] == TRUE) ? TRUE : FALSE;
 			break;
 		case 4 :
 			this->pins[5] = (links[5].second->compute(links[5].first));
 			this->pins[6] = (links[6].second->compute(links[6].first));
-			to_return = (this->pins[5] == TRUE && this->pins[6] == TRUE) ? TRUE : FALSE;
+			if (this->pins[5] == UNDEFINED || this->pins[6] == UNDEFINED)
+				to_return = UNDEFINED;
+			else
+				to_return = (this->pins[5] == TRUE && this->pins[6] == TRUE) ? TRUE : FALSE;
 			break;
 		case 10 :
 			this->pins[8] = (links[1].second->compute(links[8].first));
 			this->pins[9] = (links[2].second->compute(links[9].first));
-			to_return = (this->pins[8] == TRUE && this->pins[9] == TRUE) ? TRUE : FALSE;
+			if (this->pins[8] == UNDEFINED || this->pins[9] == UNDEFINED)
+				to_return = UNDEFINED;
+			else
+				to_return = (this->pins[8] == TRUE && this->pins[9] == TRUE) ? TRUE : FALSE;
 			break;
 		case 11 :
 			this->pins[12] = (links[1].second->compute(links[12].first));
 			this->pins[13] = (links[2].second->compute(links[13].first));
-			to_return = (this->pins[12] == TRUE && this->pins[13] == TRUE) ? TRUE : FALSE;
+			if (this->pins[12] == UNDEFINED || this->pins[13] == UNDEFINED)
+				to_return = UNDEFINED;
+			else
+				to_return = (this->pins[12] == TRUE && this->pins[13] == TRUE) ? TRUE : FALSE;
 			break;
 	}
 	this->pins[pin] = to_return;
